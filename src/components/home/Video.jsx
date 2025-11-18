8be13395ebd77623f8afbf92ea22d39f3d0d2aef
VISHALVARUN011002 <vishalkumarvarun01@gmail.com>
2025-11-18 14:27:38 +0530

    change dist and video path

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -4,11 +4,15 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video 
-      className='h-full w-full object-cover' 
-         autoPlay loop muted
-         src="../../../public/video.mp4"
-      ></video>
+     <video
+  className="h-full w-full object-cover"
+  autoPlay
+  loop
+  muted
+  src={`${import.meta.env.BASE_URL}video.mp4`}
+>
+</video>
+
     </div>
   )
 
